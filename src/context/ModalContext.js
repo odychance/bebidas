c19be869475ef77c3a3165f3e10022d4bf409f1e
odychance <odychance96@gmail.com>
2023-05-07 13:54:23 -0300

    agregue el useeffect

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -6,7 +6,20 @@ export const ModalContext = createContext()
 const ModalProvider = (props) => {
 
     const [idReceta, setIdReceta] = useState(null)
+    const [informacion, setReceta] = useState({})
 
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!idReceta) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`
+
+            const resultado = await axios.get(url)
+
+            setReceta(resultado.data.drinks[0])
+        }
+        obtenerReceta()
+    }, [idReceta])
 
     return( 
         <ModalContext.Provider
