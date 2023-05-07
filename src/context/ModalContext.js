53e98b40db2e726064472f2b81a13be1f76268a1
odychance <odychance96@gmail.com>
2023-05-07 13:53:48 -0300

    agregue una linea

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -7,18 +7,6 @@ const ModalProvider = (props) => {
 
     const [idReceta, setIdReceta] = useState(null)
 
-    useEffect(() => {
-        const obtenerReceta = async () => {
-            if(!idReceta) return;
-
-            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`
-
-            const resultado = await axios.get(url)
-
-            setReceta(resultado.data.drinks[0])
-        }
-        obtenerReceta()
-    }, [idReceta])
 
     return( 
         <ModalContext.Provider
